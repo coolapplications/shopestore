989b29ee13f6716572a29e75132d777c0a64c383
n0vat0 <amauricio.alvarez@udea.edu.co>
2019-09-26 11:52:22 -0500

    some changes

--- controllers/postHandler.js
+++ controllers/postHandler.js
@@ -1,8 +1,22 @@
 
 'use strict'
 module.exports = function (router) {
+
+  function empty_fields(body){
+    return body.userName === '' || body.email === '' || body.password === '' || body.passwordConfirmation === ''
+  }
+
+  function valid_email(body){
+    var email_pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    return email_pattern.test(body.email)
+  }
+
+  function password_coincide(body){
+    return body.password === body.passwordConfirmation
+  }
   router.post('/pages/registrar', function (req, res) {
     console.log(req.body)
-    res.send('got it')
+    res.render('/pages/registrar')
   })
 }
+
---
+++ dataBase/dataBase.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = {
+    database: {
+        
+        host: 'www.db4free.net',
+        user: 'formoldog',
+        password: 'seattle3317',
+        database: 'santiagootalvaro',
+        port: '3306'
+    }
+};
