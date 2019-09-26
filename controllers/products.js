909ab656a07a17a82e8fecc100fca7324035cd8d
Andrés Alvarez <amauricio.alvarez@udea.edu.co>
2019-09-26 08:15:52 -0500

    Fixes for netlify deploy

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,12 @@
 'use strict';
-const pool = require('../dataBase/dataBase');
-module.exports = function(router){
-	router.get('/', function(req, res){
-		res.render('index');
-	});
+//const pool = require('../dataBase/dataBase');
+module.exports = function (router) {
+    router.get('/', function (req, res) {
+        res.render('index');
+    });
 
 
-	router.get('/detalles', function(req, res){
-		res.render('products/detalles');
-	});
+    router.get('/detalles', function (req, res) {
+        res.render('products/detalles');
+    });
 }
