76b85cc6650a89d372617e767a6faae655245ec6
Andrés Alvarez <amauricio.alvarez@udea.edu.co>
2019-09-28 08:47:33 -0500

    tryout1

--- .build/js/main.js
+++ .build/js/main.js
@@ -1,139 +1,143 @@
-$(function() {
-    "use strict";
-  
+/* eslint-disable no-undef */
+$(function () {
+    'use strict';
+
     //------- Parallax -------//
     skrollr.init({
-      forceHeight: false
+        forceHeight: false
     });
-  
+
     //------- Active Nice Select --------//
     $('select').niceSelect();
-  
+
     //------- hero carousel -------//
-    $(".hero-carousel").owlCarousel({
-      items:3,
-      margin: 10,
-      autoplay:false,
-      autoplayTimeout: 5000,
-      loop:true,
-      nav:false,
-      dots:false,
-      responsive:{
-        0:{
-          items:1
-        },
-        600:{
-          items: 2
-        },
-        810:{
-          items:3
-        }
-      }
-    });
-  
-    //------- Best Seller Carousel -------//
-    if($('.owl-carousel').length > 0){
-      $('#bestSellerCarousel').owlCarousel({
-        loop:true,
-        margin:30,
-        nav:true,
-        navText: ["<i class='ti-arrow-left'></i>","<i class='ti-arrow-right'></i>"],
+    $('.hero-carousel').owlCarousel({
+        items: 3,
+        margin: 10,
+        autoplay: false,
+        autoplayTimeout: 5000,
+        loop: true,
+        nav: false,
         dots: false,
-        responsive:{
-          0:{
-            items:1
-          },
-          600:{
-            items: 2
-          },
-          900:{
-            items:3
-          },
-          1130:{
-            items:4
-          }
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            810: {
+                items: 3
+            }
         }
-      })
+    });
+
+    //------- Best Seller Carousel -------//
+    if ($('.owl-carousel').length > 0) {
+        $('#bestSellerCarousel').owlCarousel({
+            loop: true,
+            margin: 30,
+            nav: true,
+            navText: ['<i class=\'ti-arrow-left\'></i>', '<i class=\'ti-arrow-right\'></i>'],
+            dots: false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                900: {
+                    items: 3
+                },
+                1130: {
+                    items: 4
+                }
+            }
+        });
     }
-  
+
     //------- single product area carousel -------//
-    $(".s_Product_carousel").owlCarousel({
-      items:1,
-      autoplay:false,
-      autoplayTimeout: 5000,
-      loop:true,
-      nav:false,
-      dots:false
+    $('.s_Product_carousel').owlCarousel({
+        items: 1,
+        autoplay: false,
+        autoplayTimeout: 5000,
+        loop: true,
+        nav: false,
+        dots: false
     });
-  
-    //------- mailchimp --------//  
-      function mailChimp() {
-          $('#mc_embed_signup').find('form').ajaxChimp();
-      }
+
+    //------- mailchimp --------//
+    function mailChimp() {
+        $('#mc_embed_signup').find('form').ajaxChimp();
+    }
     mailChimp();
-    
-    //------- fixed navbar --------//  
-    $(window).scroll(function(){
-      var sticky = $('.header_area'),
-      scroll = $(window).scrollTop();
-  
-      if (scroll >= 100) sticky.addClass('fixed');
-      else sticky.removeClass('fixed');
+
+    //------- fixed navbar --------//
+    $(window).scroll(function () {
+        var sticky = $('.header_area'),
+            scroll = $(window).scrollTop();
+
+        if (scroll >= 100) sticky.addClass('fixed');
+        else sticky.removeClass('fixed');
     });
-  
+
     //------- Price Range slider -------//
-    if(document.getElementById("price-range")){
-    
-      var nonLinearSlider = document.getElementById('price-range');
-      
-      noUiSlider.create(nonLinearSlider, {
-          connect: true,
-          behaviour: 'tap',
-          start: [ 500, 4000 ],
-          range: {
-              // Starting at 500, step the value by 500,
-              // until 4000 is reached. From there, step by 1000.
-              'min': [ 0 ],
-              '10%': [ 500, 500 ],
-              '50%': [ 4000, 1000 ],
-              'max': [ 10000 ]
-          }
-      });
-    
-    
-      var nodes = [
-          document.getElementById('lower-value'), // 0
-          document.getElementById('upper-value')  // 1
-      ];
-    
-      // Display the slider value and how far the handle moved
-      // from the left edge of the slider.
-      nonLinearSlider.noUiSlider.on('update', function ( values, handle, unencoded, isTap, positions ) {
-          nodes[handle].innerHTML = values[handle];
-      });
-    
+    if (document.getElementById('price-range')) {
+
+        var nonLinearSlider = document.getElementById('price-range');
+
+        noUiSlider.create(nonLinearSlider, {
+            connect: true,
+            behaviour: 'tap',
+            start: [500, 4000],
+            range: {
+                // Starting at 500, step the value by 500,
+                // until 4000 is reached. From there, step by 1000.
+                'min': [0],
+                '10%': [500, 500],
+                '50%': [4000, 1000],
+                'max': [10000]
+            }
+        });
+
+
+        var nodes = [
+            document.getElementById('lower-value'), // 0
+            document.getElementById('upper-value') // 1
+        ];
+
+        // Display the slider value and how far the handle moved
+        // from the left edge of the slider.
+        nonLinearSlider.noUiSlider.on('update', function (values, handle) {
+            nodes[handle].innerHTML = values[handle];
+        });
+
     }
 
 
-      // setup route middlewares
-      var csrfProtection = csrf({ cookie: true })
-      var parseForm = bodyParser.urlencoded({ extended: false })
+    // setup route middlewares
+    var csrfProtection = csrf({
+        cookie: true
+    });
+    var parseForm = bodyParser.urlencoded({
+        extended: false
+    });
 
-      // create express app
-      var app = express()
+    // create express app
+    var app = express();
 
-      // parse cookies
-      // we need this because "cookie" is true in csrfProtection
-      app.use(cookieParser())
+    // parse cookies
+    // we need this because "cookie" is true in csrfProtection
+    app.use(cookieParser());
 
-      //app.get('/form', csrfProtection, function (req, res) {
-      // pass the csrfToken to the view
-      //res.render('send', { csrfToken: req.csrfToken() })
-      //})
+    //app.get('/form', csrfProtection, function (req, res) {
+    // pass the csrfToken to the view
+    //res.render('send', { csrfToken: req.csrfToken() })
+    //})
 
-      app.post('/registrar', parseForm, csrfProtection, function (req, res) {
+    app.post('/registrar', parseForm, csrfProtection, function (req, res) {
         console.log(req.body);
-        res.send('data is being processed')
-      })
-  });
-  
+        res.send('data is being processed');
+    });
+});
